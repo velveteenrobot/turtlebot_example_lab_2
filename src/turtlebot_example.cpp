02ba0fd8590d63d3256a750f71ffbee3289865b6
leigh <leigh@leigh-stealth-linux.(none)>
2013-10-25 00:17:51 -0400

    Got mapping working, drawing the walls

--- src/turtlebot_example.cpp
+++ src/turtlebot_example.cpp
@@ -12,11 +12,17 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
 #include <tf/transform_datatypes.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <turtlebot_example/ips_msg.h>
+
+
+#include <vector>
+
+using namespace std;
 
 ros::Publisher marker_pub;
 
@@ -26,103 +32,197 @@ ros::Publisher marker_pub;
 
 void pose_callback(const turtlebot_example::ips_msg& msg)
 {
-	//This function is called when a new position message is received
-	if(msg.tag_id != TAGID) {
-            return;
-	}
+  //This function is called when a new position message is received
+  if(msg.tag_id != TAGID) {
+    return;
+  }
 
-	double X = msg.X; // Robot X psotition
-	double Y = msg.Y; // Robot Y psotition
- 	double Yaw = msg.Yaw; // Robot Yaw
+  double X = msg.X; // Robot X psotition
+  double Y = msg.Y; // Robot Y psotition
+  double Yaw = msg.Yaw; // Robot Yaw
 
-	std::cout << "X: " << X << ", Y: " << Y << ", Yaw: " << Yaw << std::endl ;
+  // std::cout << "X: " << X << ", Y: " << Y << ", Yaw: " << Yaw << std::endl ;
 }
 
 //Example of drawing a curve
-void drawCurve(int k) 
-{
-   // Curves are drawn as a series of stright lines
-   // Simply sample your curves into a series of points
+void drawCurve(int k) {
+  // Curves are drawn as a series of stright lines
+  // Simply sample your curves into a series of points
 
-   double x = 0;
-   double y = 0;
-   double steps = 50;
+  double x = 0;
+  double y = 0;
+  double steps = 50;
 
-   visualization_msgs::Marker lines;
-   lines.header.frame_id = "/map";
-   lines.id = k; //each curve must have a unique id or you will overwrite an old ones
-   lines.type = visualization_msgs::Marker::LINE_STRIP;
-   lines.action = visualization_msgs::Marker::ADD;
-   lines.ns = "curves";
-   lines.scale.x = 0.1;
-   lines.color.r = 1.0;
-   lines.color.b = 0.2*k;
-   lines.color.a = 1.0;
+  visualization_msgs::Marker lines;
+  lines.header.frame_id = "/map";
+  lines.id = k; //each curve must have a unique id or you will overwrite an old ones
+  lines.type = visualization_msgs::Marker::LINE_STRIP;
+  lines.action = visualization_msgs::Marker::ADD;
+  lines.ns = "curves";
+  lines.scale.x = 0.1;
+  lines.color.r = 1.0;
+  lines.color.b = 0.2*k;
+  lines.color.a = 1.0;
 
-   //generate curve points
-   for(int i = 0; i < steps; i++) {
-       geometry_msgs::Point p;
-       p.x = x;
-       p.y = y;
-       p.z = 0; //not used
-       lines.points.push_back(p); 
+  //generate curve points
+  for(int i = 0; i < steps; i++) {
+    geometry_msgs::Point p;
+    p.x = x;
+    p.y = y;
+    p.z = 0; //not used
+    lines.points.push_back(p);
 
-       //curve model
-       x = x+0.1;
-       y = sin(0.1*i*k);   
-   }
+    //curve model
+    x = x+0.1;
+    y = sin(0.1*i*k);
+  }
 
-   //publish new curve
-   marker_pub.publish(lines);
+  //publish new curve
+  marker_pub.publish(lines);
 
+}
+
+void drawPoint(float x, float y) {
+  static int lastId = 1;
+
+  visualization_msgs::Marker lines;
+  lines.header.frame_id = "/map";
+  lines.id = lastId; //each curve must have a unique id or you will overwrite an old ones
+  lastId++;
+  lines.type = visualization_msgs::Marker::LINE_STRIP;
+  lines.action = visualization_msgs::Marker::ADD;
+  lines.ns = "curves";
+  lines.scale.x = 0.1;
+  lines.color.r = 1.0;
+  lines.color.b = 0.2;
+  lines.color.a = 1.0;
+
+  geometry_msgs::Point p;
+  p.x = x;
+  p.y = y;
+  p.z = 0; //not used
+  lines.points.push_back(p);
+  p.x = x + 0.1;
+  p.y = y + 0.1;
+  p.z = 0; //not used
+  lines.points.push_back(p);
+
+  //publish new curve
+  marker_pub.publish(lines);
+}
+
+
+class Map {
+ public:
+  Map(const nav_msgs::OccupancyGrid& msg);
+  bool isOccupied(float x, float y);
+
+  void drawPositions();
+
+ private:
+  unsigned int mWidth, mHeight;
+  float mResolution;
+  vector<vector<bool>* > mMap;
+  geometry_msgs::Pose mPose;
+
+  bool isCellIndexOccupied(unsigned int xGrid, unsigned int yGrid);
+};
+
+Map::Map(const nav_msgs::OccupancyGrid& msg) {
+  mPose = msg.info.origin;
+  mWidth = msg.info.width;
+  mHeight = msg.info.height;
+  mResolution = msg.info.resolution;
+
+  vector<signed char, allocator<signed char> >::const_iterator iter = msg.data.begin();
+
+  // copy over the map data
+  for (int y = 0; y < mHeight; ++y) {
+    mMap.push_back(new vector<bool>());
+    for (int x = 0; x < mWidth; ++x) {
+      bool occupied = (*iter) > 50;
+      mMap[y]->push_back(occupied);
+      iter++;
+    }
+  }
+}
+
+bool Map::isOccupied(float x, float y) {
+  // convert xy to grid position
+  int xGrid = (int)((x - mPose.position.x) / mResolution);
+  int yGrid = (int)((y - mPose.position.y) / mResolution);
+  if (xGrid < 0 ||
+      xGrid >= mWidth ||
+      yGrid < 0 ||
+      yGrid >= mHeight) {
+    std::cout<<"Invalid map position: "<<x<<", "<<y<<std::endl;
+  }
+  return isCellIndexOccupied(xGrid, yGrid);
+}
+
+bool Map::isCellIndexOccupied(unsigned int xGrid, unsigned int yGrid) {
+  return (*mMap[yGrid])[xGrid];
+}
+
+void Map::drawPositions() {
+  // copy over the map data
+  for (int y = 0; y < mHeight; ++y) {
+    mMap.push_back(new vector<bool>());
+    for (int x = 0; x < mWidth; ++x) {
+      if (isCellIndexOccupied(x, y)) {
+        cout<<"occupied!"<<endl;
+        drawPoint(
+            (x * mResolution) + mPose.position.x,
+            (y * mResolution) + mPose.position.y);
+      } else {
+        cout<<"NOT occupied!"<<endl;
+      }
+    }
+  }
 }
 
 //Callback function for the map
 void map_callback(const nav_msgs::OccupancyGrid& msg)
 {
-    //This function is called when a new map is received
-    
-    //you probably want to save the map into a form which is easy to work with
-}
+  //This function is called when a new map is received
+  //you probably want to save the map into a form which is easy to work with
 
+  Map map(msg);
+  map.drawPositions();
+  // TODO: calculate the path
+}
 
 int main(int argc, char **argv)
 {
-	//Initialize the ROS framework
-    ros::init(argc,argv,"main_control");
-    ros::NodeHandle n;
+  //Initialize the ROS framework
+  ros::init(argc,argv,"main_control");
+  ros::NodeHandle n;
 
-    //Subscribe to the desired topics and assign callbacks
-    ros::Subscriber map_sub = n.subscribe("/map", 1, map_callback);
-		ros::Subscriber pose_sub = n.subscribe("/indoor_pos", 1, pose_callback);
+  //Subscribe to the desired topics and assign callbacks
+  ros::Subscriber map_sub = n.subscribe("/map", 1, map_callback);
+  ros::Subscriber pose_sub = n.subscribe("/indoor_pos", 1, pose_callback);
 
-    //Setup topics to Publish from this node
-    ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
-    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
+  //Setup topics to Publish from this node
+  ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
+  marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
+
+  //Velocity control variable
+  geometry_msgs::Twist vel;
+
+  //Set the loop rate
+  ros::Rate loop_rate(20);    //20Hz update rate
+
+  while (ros::ok())
+  {
+    loop_rate.sleep(); //Maintain the loop rate
+    ros::spinOnce();   //Check for new messages
     
-    //Velocity control variable
-    geometry_msgs::Twist vel;
+    //Main loop code goes here:
+    // vel.linear.x = 0.1; // set linear speed
+    // vel.angular.z = 0.3; // set angular speed
 
-    //Set the loop rate
-    ros::Rate loop_rate(20);    //20Hz update rate
-	
+    velocity_publisher.publish(vel); // Publish the command velocity
+  }
 
-    while (ros::ok())
-    {
-    	loop_rate.sleep(); //Maintain the loop rate
-    	ros::spinOnce();   //Check for new messages
-
-	 //Draw Curves
-         drawCurve(1);
-         drawCurve(2);
-         drawCurve(4);
-    
-    	//Main loop code goes here:
-    	vel.linear.x = 0.1; // set linear speed
-    	vel.angular.z = 0.3; // set angular speed
-
-    	velocity_publisher.publish(vel); // Publish the command velocity
-    }
-
-    return 0;
+  return 0;
 }
